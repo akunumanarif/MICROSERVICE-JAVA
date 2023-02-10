f5b55ba6ff526bfaf79bc3cba04e6f2d53148227
M Nu'man Arif H <numanarif87@gmail.com>
2023-02-10 20:23:46 +0700

    Add Book Service

---
+++ books/src/test/java/com/service/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
