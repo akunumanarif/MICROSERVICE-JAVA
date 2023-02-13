27f210b0b21dfc12c064df5acff6489f6336c57e
M Nu'man Arif H <numanarif87@gmail.com>
2023-02-13 17:00:11 +0700

    Add API Doc with Swagger in http://localhost:8080/swagger-ui/index.html

--- books/src/main/java/com/service/books/BooksApplication.java
+++ books/src/main/java/com/service/books/BooksApplication.java
@@ -1,9 +1,13 @@
 package com.service.books;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableAutoConfiguration
 public class BooksApplication {
 
 	public static void main(String[] args) {
